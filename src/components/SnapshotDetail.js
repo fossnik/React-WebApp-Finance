d350a73de89530c3915610722837ba513ba55dd7
Zack Hartmann <zackhartmann@gmail.com>
2018-05-04 11:50:31 -0400

    Implemented Vertical Table

--- src/components/SnapshotDetail.js
+++ src/components/SnapshotDetail.js
@@ -8,35 +8,53 @@ const SnapshotDetail = (props) => {
 	return (
 		<div className="Table-container">
 			<table className="Table">
-				<thead className="Table-head">
-				<tr>
-					<th>ID</th>
-					<th>Date Created</th>
-					<th>Symbol</th>
-					<th>Name</th>
-					<th>Price</th>
-					<th>Change</th>
-					<th>Percent Change</th>
-					<th>Market Cap</th>
-					<th>Volume</th>
-					<th>Volume 24h</th>
-					<th>Total Volume 24h</th>
-					<th>Circulating Suppy</th>
-				</tr>
-				</thead>
 				<tbody className="Table-body">
-					<tr key={coin.MarketID}>
+					<tr>
+						<td>ID</td>
 						<td><span>{coin.ID}</span></td>
+					</tr>
+					<tr>
+						<td>Date Created</td>
 						<td><span>{coin.dateCreated}</span></td>
+					</tr>
+					<tr>
+						<td>Symbol</td>
 						<td><span>{coin.symbol}</span></td>
+					</tr>
+					<tr>
+						<td>Name</td>
 						<td><span>{coin.name}</span></td>
+					</tr>
+					<tr>
+						<td>Price</td>
 						<td><span>{coin.price}</span></td>
+					</tr>
+					<tr>
+						<td>Change</td>
 						<td><span>{coin.change}</span></td>
+					</tr>
+					<tr>
+						<td>Percent Change</td>
 						<td><span>{coin.pChange}</span></td>
+					</tr>
+					<tr>
+						<td>Market Cap</td>
 						<td><span>{coin.marketCap}</span></td>
+					</tr>
+					<tr>
+						<td>Volume</td>
 						<td><span>{coin.volume}</span></td>
+					</tr>
+					<tr>
+						<td>Volume 24h</td>
 						<td><span>{coin.volume24h}</span></td>
+					</tr>
+					<tr>
+						<td>Total Volume 24h</td>
 						<td><span>{coin.totalVolume24h}</span></td>
+					</tr>
+					<tr>
+						<td>Circulating Suppy</td>
 						<td><span>{coin.circulatingSupply}</span></td>
 					</tr>
 				</tbody>
