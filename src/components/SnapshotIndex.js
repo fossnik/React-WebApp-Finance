50fe1999bb87dc0f5567588dbf675bd03ce79b94
Zack Hartmann <zackhartmann@gmail.com>
2018-05-04 00:33:03 -0400

    Refactored SnapshotIndex to Component Class

--- src/components/SnapshotIndex.js
+++ src/components/SnapshotIndex.js
@@ -1,36 +1,39 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import '../common/Table.css'
 
-const CoinMenuList = (props) => {
-	const { result } = props;
+class SnapshotIndex extends Component {
+	render() {
+		const {result} = this.props;
 
-	return (
-		<div className="Table-container">
-			<table className="Table">
-				<thead className="Table-head">
-				<tr>
-					<th>Coins</th>
-				</tr>
-				</thead>
-				<tbody className="Table-body">
-				{result.map(snapshot => (
-					<tr key={snapshot.ID}>
-						<td>
-							<span>
-								<a href={snapshot.ID}>{snapshot.dateCreated}</a>
-							</span>
-						</td>
+		return (
+			<div className="Table-container">
+				<table className="Table">
+					<thead className="Table-head">
+					<tr>
+						<th>Coins</th>
 					</tr>
-				))}
-				</tbody>
-			</table>
-		</div>
-	)
+					</thead>
+					<tbody className="Table-body">
+					{result.snaps.map(snapshot => (
+						<tr
+							key={snapshot.ID}
+							onClick={this.props.action}
+						>
+							<td>
+								<span>{snapshot.dateCreated}</span>
+							</td>
+						</tr>
+					))}
+					</tbody>
+				</table>
+			</div>
+		)
+	}
+}
+
+SnapshotIndex.propTypes = {
+	result: PropTypes.object.isRequired,
 };
 
-CoinMenuList.propTypes = {
-	result: PropTypes.array.isRequired,
-};
-
-export default CoinMenuList
+export default SnapshotIndex
