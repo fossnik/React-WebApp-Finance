9696dfaa66c654b63d65a495ea375c2452b15030
Zack Hartmann <zackhartmann@gmail.com>
2018-05-08 14:24:42 -0400

    Disabled NaviBox

--- src/App.js
+++ src/App.js
@@ -80,7 +80,7 @@ export default class App extends Component {
 
 	render() {
 		return <div>
-				<NaviBox state={this.state}/>
+				{/*<NaviBox state={this.state}/>*/}
 				<Table
 					resultFirst={this.state.resultFirst}
 					resultSecond={this.state.resultSecond}
