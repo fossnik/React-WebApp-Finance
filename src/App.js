541f969efaba6271a6116068d6eb1e64dbdfb540
Zack Hartmann <zackhartmann@gmail.com>
2018-05-09 13:40:03 -0400

    CSS

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import Table from './common/Table'
-import NaviBox from './common/NaviBox'
+// import NaviBox from './common/NaviBox'
 import { API_URL } from './config'
 
 export default class App extends Component {
@@ -80,8 +80,8 @@ export default class App extends Component {
 
 	render() {
 		return <div>
-				{/*<NaviBox state={this.state}/>*/}
-				<Table
+			{/*<NaviBox state={this.state}/>*/}
+			<Table
 					resultFirst={this.state.resultFirst}
 					resultSecond={this.state.resultSecond}
 					resultThird={this.state.resultThird}
