5cd8f9e1ab99b5438528b4e432bfefa09a77b39e
Zack Hartmann <zackhartmann@gmail.com>
2018-05-09 11:20:59 -0400

    Implemented StartButton Class

---
+++ src/components/StartButton.js
@@ -0,0 +1,8 @@
+import React from "react"
+import '../common/Button.css'
+
+const StartButton = () => {
+	return <button className="StartButton">Start</button>
+};
+
+export default StartButton
--- src/index.js
+++ src/index.js
@@ -2,21 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import About from './About'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import StartButton from './components/StartButton'
 
-ReactDOM.render(startButton(), document.getElementById('startButton'));
-ReactDOM.render(homePage(), document.getElementById('root'));
+ReactDOM.render(<StartButton/>, document.querySelector('.StartButton'));
+ReactDOM.render(<About/>, document.getElementById('root'));
 
 // app start button
-document.getElementById('startButton').onclick = () => {
-	document.getElementById('startButton').style.visibility = "hidden";
+document.querySelector('.StartButton').onclick = () => {
+	document.querySelector('.StartButton').style.visibility = "hidden";
 	ReactDOM.render(<App/>, document.getElementById('root'));
 };
-
-function homePage() {
-	return <About/>
-}
-
-function startButton() {
-	return <button id="startButton" className="btn btn-primary btn-lg">Start</button>
-}
