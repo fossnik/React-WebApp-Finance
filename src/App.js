6531251d6de01082873938b50ea019d90ebbb3a2
Zack Hartmann <zackhartmann@gmail.com>
2018-05-17 11:52:54 -0400

    Eliminated NaviBox Container

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import React, { Component } from 'react'
 import Table from './containers/Table'
-import NaviBox from './containers/navigation/NaviBox'
 
 export default class App extends Component {
 	render() {
-		return <div>
-			<NaviBox/>
-			<Table/>
-		</div>
+		return <Table/>
 	}
 }
