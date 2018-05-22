cf4dab230b533cbdab476d8fa10244682744323f
Zack Hartmann <zackhartmann@gmail.com>
2018-05-22 13:38:59 -0400

    Implemented Revised SnaphotsIndex-Index Immutability

--- src/reducers/reducer_snapshots_immutable.js
+++ src/reducers/reducer_snapshots_immutable.js
@@ -1,11 +1,13 @@
 import { APPEND_SNAPSHOT_INDEX } from "../actions"
 
-export default function(state = [], action) {
+export default function(state = {}, action) {
 	switch (action.type) {
 		case APPEND_SNAPSHOT_INDEX:
 			const symbol_safe = action.payload[0];
 			const snapDateTimes = action.payload[1];
-			return [Object.assign({}, {symbol_safe}, {snapDateTimes}), ...state];
+
+			// concat new entry to array of coin-name indexed snapshot datetimes
+			return Object.assign({}, state, {[symbol_safe]: snapDateTimes});
 
 		default:
 			return state;
