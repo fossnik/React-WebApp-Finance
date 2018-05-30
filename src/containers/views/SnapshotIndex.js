b8e3b510c5c0033ee53f2f7a9f79737770d90c7c
Zack Hartmann <zackhartmann@gmail.com>
2018-05-30 11:53:00 -0400

    Implemented Conditional Coin Index Fetch in Snapshot Index

--- src/containers/views/SnapshotIndex.js
+++ src/containers/views/SnapshotIndex.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { fetchListOfSnapshotsForSingleCoin } from "../../actions"
+import { fetchListOfSnapshotsForSingleCoin, fetchCoinIndex } from "../../actions"
 import CoinMenu from '../navigation/Menu-coins'
 
 class SnapshotIndex extends Component {
@@ -15,7 +15,11 @@ class SnapshotIndex extends Component {
 	}
 
 	componentDidMount() {
-		this.props.fetchListOfSnapshotsForSingleCoin(this.state.activeCoin)
+		this.props.fetchListOfSnapshotsForSingleCoin(this.state.activeCoin);
+
+		// load coin index index if not exist
+		if (this.props.coins === null)
+			this.props.fetchCoinIndex();
 	}
 
 	onSnapshotClick(snapId) {
@@ -25,13 +29,14 @@ class SnapshotIndex extends Component {
 	render() {
 		try {
 			const snapshotsOfThisCoin = this.props.allSnapshots.scrapeDates[this.state.activeCoin];
+			const coinFullName = this.props.coins.find(coin => coin.symbol_safe === this.state.activeCoin).name;
 
 			return <div>
 				<CoinMenu activeCoin={this.state.activeCoin} history={this.props.history}/>
 				<table className="Table">
 					<thead className="Table-head">
 					<tr>
-						<th className="Row-header-coin" colSpan='2'>{this.state.activeCoin}</th>
+						<th className="Row-header-coin" colSpan='2'>{coinFullName}</th>
 					</tr>
 					<tr>
 						<th>Selenium Web Scrape</th>
@@ -62,8 +67,9 @@ class SnapshotIndex extends Component {
 
 function mapStateToProps(state) {
 	return {
-		allSnapshots: state.allSnapshots
+		allSnapshots: state.allSnapshots,
+		coins: state.coins,
 	}
 }
 
-export default connect(mapStateToProps, {fetchListOfSnapshotsForSingleCoin})(SnapshotIndex)
+export default connect(mapStateToProps, {fetchListOfSnapshotsForSingleCoin, fetchCoinIndex})(SnapshotIndex)
